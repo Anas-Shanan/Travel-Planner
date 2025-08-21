4e4d40f21351494cbd6ef27233b9a2d0e4b99c38
Anas-Shanan <shanan.anas@yahoo.com>
2025-08-21 23:02:14 +0200

    day 6 :Favorites Page with list display.. no styling so far

--- Travel-Planner/src/Pages/FavoritesPage.jsx
+++ Travel-Planner/src/Pages/FavoritesPage.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import FavoritesList from "../components/FavoritesList";
+import { useFavorites } from "../Context/FavoritesContext";
 
 export default function FavoritesPage() {
-  return <div>this is favorites page</div>;
+  const { favorites, dispatch } = useFavorites();
+
+  return (
+    <div>
+      <h1>my favorite Countries</h1>
+      <FavoritesList favorites={favorites} dispatch={dispatch} />
+    </div>
+  );
 }
--- Travel-Planner/src/components/FavoritesList.jsx
+++ Travel-Planner/src/components/FavoritesList.jsx
@@ -1,5 +1,33 @@
 import React from "react";
 
-export default function FavoritesList() {
-  return <div>FavoritesList</div>;
+export default function FavoritesList({ favorites, dispatch }) {
+  console.log(favorites);
+
+  if (favorites.length === 0) {
+    return (
+      <div>
+        <p>No favorites yet. Add some from the country details!</p>
+      </div>
+    );
+  }
+  return (
+    <div>
+      {favorites &&
+        favorites.map((country, index) => (
+          <li key={index}>
+            {country.name.common}
+            <button
+              onClick={() =>
+                dispatch({
+                  type: "REMOVE_COUNTRY",
+                  payload: { name: country.name },
+                })
+              }
+            >
+              Remove from favorites
+            </button>
+          </li>
+        ))}
+    </div>
+  );
 }
